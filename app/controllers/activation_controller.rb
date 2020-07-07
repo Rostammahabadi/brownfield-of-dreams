eccec4d4a4fea5b5c966e47f928006c9147f1d85
Taylor Keller <takeller17@gmail.com>
2020-07-07 17:13:17 -0600

    Updates session to keep user logged in when they redirect from email link

--- app/controllers/activation_controller.rb
+++ app/controllers/activation_controller.rb
@@ -3,5 +3,6 @@ class ActivationController < ApplicationController
   def show
     @user = User.find(params[:user_id])
     @user.activate if @user.activated? == false
+    session[:user_id] = @user.id
   end
 end
